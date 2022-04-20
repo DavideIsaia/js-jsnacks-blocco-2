5b77f04be57d97738d121bb000bb0eccad6c314b
DavideIsaia <davide.enjoy@hotmail.it>
2022-04-20 14:21:46 +0200

    snack 3 scaffolding

---
+++ jsnack3/js/script.js
@@ -0,0 +1,9 @@
+// Stampa il cubo dei primi N numeri, dove N è un numero indicato dall'utente.
+
+// chiedo all'utente di inserire un numero N
+
+// creo un ciclo che va da 1 a N
+
+// all'interno del cilo faccio il cubo **3 di tutti i numeri da 1 a N
+
+// stampo il risultato
