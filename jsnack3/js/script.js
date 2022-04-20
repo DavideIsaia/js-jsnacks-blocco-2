f54b22a71c0457b986ca10bf96a9ae50cd75bdf1
DavideIsaia <davide.enjoy@hotmail.it>
2022-04-20 14:46:44 +0200

    stampo il cubo del numero inserito

--- jsnack3/js/script.js
+++ jsnack3/js/script.js
@@ -1,9 +1,20 @@
 // Stampa il cubo dei primi N numeri, dove N è un numero indicato dall'utente.
 
 // chiedo all'utente di inserire un numero N
+let userNumber = parseInt(prompt("Inserisci un numero"));
+console.log(userNumber);
 
 // creo un ciclo che va da 1 a N
+i = 1;
 
-// all'interno del cilo faccio il cubo **3 di tutti i numeri da 1 a N
+let cube = 0;
+
+while (i < userNumber) {
+
+    // all'interno del cilo faccio il cubo **3 di tutti i numeri da 1 a N
+    cube = userNumber*userNumber*userNumber; 
+    i++;
+}
 
 // stampo il risultato
+console.log(cube);
